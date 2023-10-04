3f74376ad8b70bee4a962097f841959b937b287e
VenkataSiva0326 <146852272+VenkataSiva0326@users.noreply.github.com>
2023-10-04 06:38:14 +0530

    Added Python Fundamentals

---
+++ Hello.py
@@ -0,0 +1,34 @@
+
+## Basic syntax for Hello World
+
+print("Hello we are learning Python")
+
+## Simple Calculator
+
+amount = 100
+
+tax = 0.06
+
+total = amount + amount * tax
+
+print(total)
+
+## string and input and output
+
+name = 'Venkata Siva Reddy'
+print(name)
+
+name = "Hello Venkata Siva Reddy"
+print(name)
+
+name= "venkata's Laptop is very slow"
+print(name) 
+
+
+## python input() function
+
+my_name= input("What's your name")
+print(my_name)
+
+
+
